de2a639c3be2810b58d3b21d074168ce1499440d
Harmanpreet Singh Sagar <harmanpreet1308@gmail.com>
2025-10-18 03:44:50 -0400

    Complete LC 3034 - Number of Subarrays That Match a Pattern I

--- Autodesk_OA/3034 - Number of Subarrays That Match a Pattern I/solution.py
+++ Autodesk_OA/3034 - Number of Subarrays That Match a Pattern I/solution.py
@@ -1,3 +1,30 @@
 class Solution:
     def countMatchingSubarrays(self, nums: list[int], pattern: list[int]) -> int:
-        """Stuff"""
+        pat_length = len(pattern) + 1
+        count = 0
+
+        # We only need 0 <= 1 < len(nums) - len(pattern) because otherwise the
+        # subarrays would not have enough elements to be the correct size of
+        # pattern and will give index out of bounds errors.
+        for i in range(len(nums) - len(pattern)):
+            sub_array = nums[i : i + pat_length]
+            sub_array_match = True
+
+            for k in range(len(pattern)):
+                if pattern[k] == 1:
+                    if not (sub_array[k + 1] > sub_array[k]):
+                        sub_array_match = False
+                        break
+                elif pattern[k] == 0:
+                    if sub_array[k + 1] != sub_array[k]:
+                        sub_array_match = False
+                        break
+                else:
+                    if not (sub_array[k + 1] < sub_array[k]):
+                        sub_array_match = False
+                        break
+
+            if sub_array_match:
+                count += 1
+
+        return count
