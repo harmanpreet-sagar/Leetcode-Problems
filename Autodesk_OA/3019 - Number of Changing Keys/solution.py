8849b1fea155bdf1e6886af427e4623b2f226046
Harmanpreet Singh Sagar <harmanpreet1308@gmail.com>
2025-10-18 02:44:16 -0400

    Complete LC 3019 - Number of Changing Keys

--- Autodesk_OA/3019 - Number of Changing Keys/solution.py
+++ Autodesk_OA/3019 - Number of Changing Keys/solution.py
@@ -1,3 +1,14 @@
 class Solution:
     def countKeyChanges(self, s: str) -> int:
-        """Solution"""
+        new_str = s.lower()
+        change = 0
+
+        for i in range(len(new_str)):
+            if i == 0:
+                continue
+            else:
+                if new_str[i-1] != new_str[i]:
+                    change += 1
+
+        return change
+        
